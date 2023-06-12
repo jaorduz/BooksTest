6b9032e5a01846b7bd64fa8f0df0c2c717543927
J. A. Orduz-Ducuara <jaorduz@ieee.org>
2023-06-12 08:25:22 -0400

    V01Jun122023

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # My Great Book
 
 Visit <https://pretextbook.org/documentation.html> to learn more.
+
+To make a new
+
+pretext build book
+
+To build
+pretext build web
+
+To see a web
+pretext view web
+
+To print PDF
+pretext build print
+
+To see the PDF
+pretext view print
+
+
+To image (it didn't work)
+sudo bash ./.devcontainer/postCreateCommand.sh
+
+
+Referecen:
+
+https://github.com/active-calculus/active-calculus-single-mbx
+
+https://activecalculus.org/
+
+https://pretextbook.org/doc/schema/
+
+Video:
+
+https://www.youtube.com/watch?v=y-33Ube-Rhk
+
+https://pretextbook.org/doc/guide/html/tutorial-videos.html
--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
-  "body": "   example.org   https:\/\/example.org   copyright  "
+  "body": "   jaorduz.github.io   https:\/\/jaorduz.github.io   copyright  "
 },
 {
   "id": "section-title",
@@ -16,7 +16,43 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Section title",
-  "body": " Section title  Hello  "
+  "body": " Section title  This an examples to leave objectives     How is the average velocity of a moving object connected to the values of its position function?    How do we interpret the average velocity of an object geometrically on the graph of its position function?    How is the notion of instantaneous velocity connected to average velocity?     "
+},
+{
+  "id": "ngj",
+  "level": "2",
+  "url": "section-title.html#ngj",
+  "type": "Objectives",
+  "number": "1.1",
+  "title": "",
+  "body": "   How is the average velocity of a moving object connected to the values of its position function?    How do we interpret the average velocity of an object geometrically on the graph of its position function?    How is the notion of instantaneous velocity connected to average velocity?    "
+},
+{
+  "id": "section-title2",
+  "level": "1",
+  "url": "section-title2.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "This is a section of the second chapter",
+  "body": " This is a section of the second chapter  This section is about distributions   "
+},
+{
+  "id": "section-title3a",
+  "level": "1",
+  "url": "section-title3a.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "This is a section of the third chapter",
+  "body": " This is a section of the third chapter  This section is about probability   "
+},
+{
+  "id": "section-title3b",
+  "level": "1",
+  "url": "section-title3b.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "This is  section of the third chapter",
+  "body": " This is section of the third chapter  This section is about probability part two   "
 },
 {
   "id": "colophon-2",
@@ -25,7 +61,7 @@ var ptx_lunr_docs = [
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
-  "body": " This book was authored in PreTeXt .  "
+  "body": " This book was authored in PreTeXt .   Here is the ref. . Use it!  "
 }
 ]
 
